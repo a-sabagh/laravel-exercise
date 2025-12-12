eb07bf17e221b5c92a45308cd4894bd2febda779
a-sabagh <a.sabagh72@gmail.com>
2025-12-12 14:56:14 +0330

    set lock backend store

--- tests/Unit/CacheLockTest.php
+++ tests/Unit/CacheLockTest.php
@@ -8,6 +8,8 @@ class CacheLockTest extends TestCase
 {
     public function test_cach_acquire_atomic_lock(): void
     {
+        config()->set('cache.default', 'array'); // file, null
+
         $lock = cache()->lock('foo', 10);
 
         dump('first check: ', $lock->get());
