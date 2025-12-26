628283926aee1bc630bff7d5977bf67a966420bd
Abolfazl Sabagh <a.sabagh72@gmail.com>
2025-12-26 14:41:00 +0330

    add route in cache course

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/log-info', function () {
+    defer(function () {
+        sleep(5);
+
+        Log::info('hello from apache 2');
+    });
+
+    dump('hello');
+});
+
+Route::get('post-count', function () {
+    return cache()->remember('post_count', 5, function () {
+        sleep(2);
+
+        Log::info('revalidate');
+
+        return 2500;
+    });
 });
